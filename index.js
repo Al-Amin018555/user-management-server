df2076bfedf94fe3921e0e7d47a8b2e815421990
Al-Amin018555 <alamin15-3799@diu.edu.bd>
2026-02-10 23:06:36 +0600

    new user posting featureimplemented on server

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var cors = require('cors')
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.json())
 
 const users = [
     { id: 1, name: 'shabana', email: 'shabana@gmai.com' },
@@ -17,6 +18,15 @@ app.get('/', (req, res) => {
 
 app.get('/users', (req, res) => {
     res.send(users)
+})
+
+app.post('/users', (req, res) => {
+    console.log('users post method');
+    console.log(req.body)
+    const newUser = req.body;
+    newUser.id = users.length + 1;
+    res.send(newUser)
+
 })
 
 app.listen(port, () => {
