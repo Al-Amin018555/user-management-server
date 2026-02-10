7d995dc9f79c49e0194cbded698610fc02a40197
Al-Amin018555 <alamin15-3799@diu.edu.bd>
2026-02-10 22:19:34 +0600

    users api set

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const app = express();
+var cors = require('cors')
 const port = process.env.PORT || 3000;
+
+app.use(cors());
+
+const users = [
+    { id: 1, name: 'shabana', email: 'shabana@gmai.com' },
+    { id: 2, name: 'shabnoor', email: 'shabnoor@gmai.com' },
+    { id: 3, name: 'sabila', email: 'sabila@gmai.com' },
+];
 
 app.get('/', (req, res) => {
     res.send("Users server is running")
+})
+
+app.get('/users', (req, res) => {
+    res.send(users)
 })
 
 app.listen(port, () => {
